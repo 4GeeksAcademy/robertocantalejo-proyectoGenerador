fee70a67e79fdb872f929b120e7dbf751f67b275
Roberto <robercantalejo@gmail.com>
2025-01-10 19:55:12 +0000

    Meto parámetros y algo mas

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { name } from "file-loader";
 
 window.onload = function() {
   //write your code here
@@ -16,7 +17,46 @@ let surnames = [];
 let countries = [];
 let affiliations = [];
 let traits = [];
+/* Esta función es para añadir strings a las variables declaradas antes*/
 
-/* blabkaslbdakslbd*/
+function pushInputs(
+  p_names,
+  p_surnames,
+  p_countries,
+  p_affiliations,
+  p_traits
+) {
+  const inputName = document.querySelector("#input1");
+  const inputSurname = document.querySelector("#input2");
+  const inputCountry = document.querySelector("#input3");
+  const inputAffiliation = document.querySelector("#input4");
+  const inputTrait = document.querySelector("#input5");
 
-document.quea;
+  if (inputName.value != "") {
+    p_names.push(inputName.value);
+  }
+  if (inputSurname.value !== "") {
+    p_surnames.push(inputSurname.value);
+    inputSurname.value = "";
+  }
+  if (inputCountry.value !== "") {
+    p_countries.push(inputCountry.value);
+    inputCountry.value = "";
+  }
+  if (inputAffiliation.value !== "") {
+    p_affiliations.push(inputAffiliation.value);
+    inputAffiliation.value = "";
+  }
+  if (inputTrait.value !== "") {
+    p_traits.push(inputTrait.value);
+    inputTrait.value = "";
+  }
+
+  console.log(names, surnames, countries, affiliations, traits);
+}
+
+document
+  .querySelector(".btn-primary")
+  .addEventListener("click", () =>
+    pushInputs(names, surnames, countries, affiliations, traits)
+  );
